74f9ed9d0d993b4a060b62c2eddc76880b4f79af
bersegos <bersegosx@gmail.com>
2024-05-25 18:52:17 +0500

    add few tests

--- test/berseg/tictactoe_test.clj
+++ test/berseg/tictactoe_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [berseg.tictactoe :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest next-mark-test
+  (testing
+   (are [value expected] (= expected (get-next-mark value))
+     ["X" "0"] "X"
+     ["X" "0" "X"] "0")))
+
+(deftest in-range-test
+  (testing
+   (are [expected value range-v]
+        (= expected (in-range? value range-v))
+     true 3 [1 3]
+     false 4 [1 3])))
+
+(deftest number-to-index-test
+  (testing
+   (are [value expected]
+        (= expected (number-to-index value))
+     2 [0 1]
+     6 [1 2]
+     8 [2 1])))
